9bd7240adbc7b6997f85ca25c0ada929094e29d0
Matthew Sievers <mattersievers@gmail.com>
2021-10-31 19:25:08 -0500

    offline functionality works

--- service-worker.js
+++ service-worker.js
@@ -16,6 +16,21 @@ const FILES_TO_CACHE = [
     "./dist/schedule.bundle.js"
 ];
 
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
 self.addEventListener('install', function(e){
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -33,8 +48,7 @@ self.addEventListener('activate', function(e){
             });
             cacheKeeplist.push(CACHE_NAME);
             
-            return Promise.all(
-                keyList.map(function(key, i) {
+            return Promise.all(keyList.map(function(key, i) {
                     if (cacheKeeplist.indexOf(key) === -1) {
                         console.log('deleting cache : ' + keyList[i]);
                         return caches.delete(keyList[i]);
@@ -44,18 +58,3 @@ self.addEventListener('activate', function(e){
         })
     );
 });
-
-self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-})
